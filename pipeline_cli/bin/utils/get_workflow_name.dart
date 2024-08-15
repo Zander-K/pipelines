98b596a34b6da366f9754aae6cd27bc91179f059
Zander Kotze <Zander.Kotze@rank.com>
2024-08-15 21:31:01 +0200

    chore: test gh cli

--- pipeline_cli/bin/utils/get_workflow_name.dart
+++ pipeline_cli/bin/utils/get_workflow_name.dart
@@ -1,14 +1,3 @@
 String getWorkflowName(String githubWorkflow) {
-  // final String name;
-  // var parts = githubWorkflow.split('/');
-  // final name = parts.length >= 3 ? parts[2] : 'Unknown Workflow';
-
-  // if (githubWorkflow.toLowerCase().contains('ios')) {
-  //   name = 'iOS';
-  // } else if (githubWorkflow.toLowerCase().contains('android')) {
-  //   name = 'Android';
-  // } else {
-  //   name = 'Unknown';
-  // }
   return githubWorkflow;
 }
